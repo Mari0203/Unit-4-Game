c0a53f0acf7dce40d8f25c4e72c54a5cf79fff29
Mari0203 <mari.brands@pm.me>
2019-06-21 13:41:11 -0500

    Added initial JavaScripts for generating random numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,15 @@
 // Pesudo codes //
 // Generate a random number at the start game---should be between 19 - 120. //
 var randomValue = 0;
-
 function startGame() {
-    randomValue = Math.floor(Math.random()* 10 * 100);
+    randomValue = Math.floor(Math.random()* (120-19)) + 19;
     console.log(randomValue);
 
     // Generate random values for each crystals:
     var crystal1 = Math.ceil(Math.random() * (12-1)) + 1;
-    var crystal2 = 7;
-    var crystal3 = 13;
-    var crystal4 = 25;
+    var crystal2 = Math.ceil(Math.random() * (12-1)) + 1;
+    var crystal3 = Math.ceil(Math.random() * (12-1)) + 1;
+    var crystal4 = Math.ceil(Math.random() * (12-1)) + 1;
 
     // Assign the randomly generated value of each crystal to HTML crystal id's.
      $("#crystal1").attr("data-value") = crystal1;
@@ -23,17 +22,8 @@ function startGame() {
 startGame();
 
 // When the user clicks one of the crystals, it generates a value:
-$("#crystal1").on("click", function(e) {
-    // code that display the randomly selected value for crystal1
-    
-    
-});
-
-$("#crystal2").on("click", function(e) {
-    // code that display the randomly selected value for crystal1
-    console.log($("#crystal2").attr("data-value"));
-
-    $("#crystal1").attr("data-value")
+$("#crystal1").on("click", function(e) { 
+    // It stores randomly generated value for this crystal into HTML selector.
 });
 
 // Check function: If the total value of crystal clicks is over assigned randomValue, then game over.
