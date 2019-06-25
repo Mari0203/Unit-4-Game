6edaffe806e2fb7207cc73b1d090a69f2507733e
Mari0203 <mari.brands@pm.me>
2019-06-24 23:25:15 -0500

    Added if/else if for win/loss scenarios

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,13 +2,8 @@
 // with minmum and maximum numbers being inclusive. //
 
 function getRandomNum(min, max) {
-    min = Math.ceil(min);
-    console.log("Min: " + min);
-    
+    min = Math.ceil(min); 
     max = Math.floor(max);
-    console.log("Max: " + max);
-    console.log("Max - Min: " + (max - min));
-    
     return Math.floor(Math.random() * (max - min + 1)) + min;
     // console.log("Math.random(): " + Math.random());
     // console.log("Math.random() * (max - min): " + Math.random() * (max - min));
@@ -20,24 +15,30 @@ function getRandomNum(min, max) {
 
 // Global variables
 var numToMatch = 0;
-var totalScore = 0;
+var playerScore = 0;
 var wins = 0;
 var losses = 0;
 
 function startGame() {
-    numToMatch = getRandomNum(19, 120);
-    console.log(numToMatch);
 
+    // Set (reset) the scores to zero at the start of the game
+    playerScore = 0;
+
+    numToMatch = getRandomNum(19, 120);
+    
     // Display the 'numToMatch' value in UI
-    $("#random-number").text(numToMatch);
+    $("#random-number").html(numToMatch);
 
     // Generate random values between 1 - 12 and assign them to each crystal:
     var crystal1 = getRandomNum(1, 12);
     console.log(crystal1);
+
     var crystal2 = getRandomNum(1, 12);
     console.log(crystal2);
+
     var crystal3 = getRandomNum(1, 12);
     console.log(crystal3);
+
     var crystal4 = getRandomNum(1, 12);
     console.log(crystal4);
        
@@ -48,37 +49,38 @@ function startGame() {
      $("#crystal4").attr("data-value", crystal4);
 
     // Display Scores for Total Score, Wins & Losses
-    $("#playerScore").text(totalScore);
+    $("#playerScore").text(playerScore);
     $("#wins").text(wins);
     $("#losses").text(losses);
 };
-
 
 // When the user clicks the crystals, it generates a value:
 $(".crystal").on("click", function(e) { 
 
     // It stores randomly generated value for the crystal (the element) that was clicked.
     var crystalValue = $(this).attr("data-value");
-    console.log($(this));
+    console.log("CrystalValue; " + crystalValue);
 
     // Adds the value of crystal to the playerScore and assigns it dynamically.
-    playerScore += crystalValue;
+    playerScore += parseInt(crystalValue);
+    $("#playerScore").text(playerScore);
+    console.log("Current Player Score: " + playerScore);
 
-    // Check: If the playerScore is equal to numToMatch, then update the Win #.
+    // Check: If the playerScore is equal to numToMatch, then update the Win #. If the playerScore is more than the numToMatch, then update the Loss #.
     // Restart the game after the update.
+    if (playerScore === numToMatch) {
+        wins++;
+        alert("Number matched. Congrats, you won!")
+        startGame();
     
-    
-    // Check: If the playerScore is more than the numToMatch, then update the Loss #.
-    // Restart the game after the update.
+    } else if (playerScore > numToMatch) {
+        losses++;
+        alert("Number over the limit. Sorry, you lost...");
+        startGame();
+    } 
 });
-
-
-// Check function: If the total value of crystal clicks is over assigned randomValue, then game over.
-// If below, user can continue click.
 
 // Make functions available to be executed when the document is loaded (triggers the event):
 $(document).ready(function() {
     startGame();
 });
-
-//===== Reset the game ===== >> Clear the values and start over //
