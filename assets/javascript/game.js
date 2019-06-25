50d4b43975103ec568d60dd2940ff4b75f9bdeff
Mari0203 <mari.brands@pm.me>
2019-06-24 22:17:12 -0500

    Updated JS - Math.random() function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,8 +3,16 @@
 
 function getRandomNum(min, max) {
     min = Math.ceil(min);
+    console.log("Min: " + min);
+    
     max = Math.floor(max);
+    console.log("Max: " + max);
+    console.log("Max - Min: " + (max - min));
+    
     return Math.floor(Math.random() * (max - min + 1)) + min;
+    // console.log("Math.random(): " + Math.random());
+    // console.log("Math.random() * (max - min): " + Math.random() * (max - min));
+    // console.log("getRandomNum return output: " + Math.floor[Math.random() * (max - min + 1)] + min);
 };
 
 // ===== At the start of the game ===== //
@@ -14,7 +22,6 @@ var randomValue = 0;
 
 function startGame() {
     randomValue = getRandomNum(19, 120);
-    // randomValue = Math.floor(Math.random() * (120-19)) + 19;
     console.log(randomValue);
 
     // Generate random values between 1 - 12 and assign them to each crystal:
@@ -22,28 +29,27 @@ function startGame() {
     var crystal2 = getRandomNum(1, 12);
     var crystal3 = getRandomNum(1, 12);
     var crystal4 = getRandomNum(1, 12);
-    
-    // var crystal1 = Math.ceil(Math.random() * (12-1)) + 1;
-    // var crystal2 = Math.ceil(Math.random() * (12-1)) + 1;
-    // var crystal3 = Math.ceil(Math.random() * (12-1)) + 1;
-    // var crystal4 = Math.ceil(Math.random() * (12-1)) + 1;
-
+    console.log(crystal4);
+       
     // Assign the randomly generated value of each crystal to HTML crystal id's.
-     $("#crystal1").attr("data-value") = crystal1;
-     $("#crystal2").attr("data-value") = crystal2;
-     $("#crystal3").attr("data-value") = crystal3;
-     $("#crystal4").attr("data-value") = crystal4;
+     $("#crystal1").attr("data-value", crystal1);
+     $("#crystal2").attr("data-value", crystal2);
+     $("#crystal3").attr("data-value", crystal3);
+     $("#crystal4").attr("data-value", crystal4);
 };
-
-// Call the function
-startGame();
 
 // When the user clicks one of the crystals, it generates a value:
 $("#crystal1").on("click", function(e) { 
     // It stores randomly generated value for this crystal into HTML selector.
+    crystal1 = $("#crystal1").attr("data-value");
 });
 
 // Check function: If the total value of crystal clicks is over assigned randomValue, then game over.
 // If below, user can continue click.
 
-// Reset the game >> clear the values and start over. 
+// Make functions available to be executed when the document is loaded (triggers the event):
+$(document).ready(function() {
+    startGame();
+});
+
+//===== Reset the game ===== >> Clear the values and start over //
