62c40e01aadb9d7a88f4f0e7f0b7e8dd28fcd165
Mari0203 <mari.brands@pm.me>
2019-06-24 22:47:05 -0500

    Added on click event in JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,16 +18,26 @@ function getRandomNum(min, max) {
 // ===== At the start of the game ===== //
 // Generate a random number for the player to guess, and also assign randomly generated numbers for each crystal.
 
-var randomValue = 0;
+// Global variables
+var numToMatch = 0;
+var totalScore = 0;
+var wins = 0;
+var losses = 0;
 
 function startGame() {
-    randomValue = getRandomNum(19, 120);
-    console.log(randomValue);
+    numToMatch = getRandomNum(19, 120);
+    console.log(numToMatch);
+
+    // Display the 'numToMatch' value in UI
+    $("#random-number").text(numToMatch);
 
     // Generate random values between 1 - 12 and assign them to each crystal:
     var crystal1 = getRandomNum(1, 12);
+    console.log(crystal1);
     var crystal2 = getRandomNum(1, 12);
+    console.log(crystal2);
     var crystal3 = getRandomNum(1, 12);
+    console.log(crystal3);
     var crystal4 = getRandomNum(1, 12);
     console.log(crystal4);
        
@@ -36,13 +46,32 @@ function startGame() {
      $("#crystal2").attr("data-value", crystal2);
      $("#crystal3").attr("data-value", crystal3);
      $("#crystal4").attr("data-value", crystal4);
+
+    // Display Scores for Total Score, Wins & Losses
+    $("#playerScore").text(totalScore);
+    $("#wins").text(wins);
+    $("#losses").text(losses);
 };
 
-// When the user clicks one of the crystals, it generates a value:
-$("#crystal1").on("click", function(e) { 
-    // It stores randomly generated value for this crystal into HTML selector.
-    crystal1 = $("#crystal1").attr("data-value");
+
+// When the user clicks the crystals, it generates a value:
+$(".crystal").on("click", function(e) { 
+
+    // It stores randomly generated value for the crystal (the element) that was clicked.
+    var crystalValue = $(this).attr("data-value");
+    console.log($(this));
+
+    // Adds the value of crystal to the playerScore and assigns it dynamically.
+    playerScore += crystalValue;
+
+    // Check: If the playerScore is equal to numToMatch, then update the Win #.
+    // Restart the game after the update.
+    
+    
+    // Check: If the playerScore is more than the numToMatch, then update the Loss #.
+    // Restart the game after the update.
 });
+
 
 // Check function: If the total value of crystal clicks is over assigned randomValue, then game over.
 // If below, user can continue click.
